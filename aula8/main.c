fd7400de581b6e97525275743650622c23fe1517
Arthur Uliana de Angelo <arthuruliana@hotmail.com>
2026-06-23 22:23:36 -0300

    aula 8 e 9 completas

--- aula8/main.c
+++ aula8/main.c
@@ -2,73 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "hash.h"
-#include "list.h"
-#include "vector.h"
-#include "node.h"
-
-int hash_fn(HashTable *h, void *data)
+int hash_fn(void *data, int table_size)
 {
     char *str = (char *)data;
-    int hash = 0;
-    int base = 127;
+    int hash_val = 0, base = 27183;
+    int random_val = 31415;
     int size = strlen(str);
 
     for (int i = 0; i < size; i++)
     {
-        hash = (base * hash + str[i]) % hash_table_size(h);
+        hash_val = (hash_val * random_val + str[i]) % table_size;
+        random_val = (random_val * base) % (table_size - 1);
     }
 
-    return hash;
-}
-
-int cmp_str(void *a, void *b)
-{
-    return strcmp((char *)a, (char *)b);
-}
-
-int cmp_vector(void *a, void *b)
-{
-    HashTableItem *item_a = (HashTableItem *)a;
-    HashTableItem *item_b = (HashTableItem *)b;
-
-    return cmp_str(item_a->key, item_b->key);
+    return hash_val;
 }
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
+    int seed, update, table_size, n;
+    scanf("%d %d %d %d", &seed, &update, &table_size, &n);
 
-    HashTable *h = hash_table_construct(n, hash_fn, cmp_str);
-
+    char str[30];
     for (int i = 0; i < n; i++)
     {
-        char *str = (char *)malloc(30 * sizeof(char));
         scanf(" %s", str);
 
-        void *prev_val = hash_table_get(h, str);
-        if (prev_val == NULL)
-        {
-            int *new_val = (int *)malloc(sizeof(int));
-            *new_val = 1;
-            hash_table_set(h, str, new_val);
-        }
-        else
-        {
-            (*(int *)prev_val)++;
-            hash_table_set(h, str, prev_val);
-        }
+        printf("%d\n", hash_fn(str, table_size));
     }
-
-    Vector *v = hash_to_vector(h);
-    vector_sort(v, cmp_vector);
-    for (int i = 0; i < vector_size(v); i++)
-    {
-        HashTableItem *item = (HashTableItem *)vector_get(v, i);
-        printf("%s %d\n", (char *)item->key, *(int *)item->val);
-    }
-
-    vector_destroy(v);
-    hash_table_destroy(h);
 }
